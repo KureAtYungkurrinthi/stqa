0d8f4b9038b4242eb0e39057a3a59b0d3d3aaea4
Wang Shengfan <wang2147@flinders.edu.au>
2024-04-28 18:00:25 +0930

    Task 4 Complete Factorial class main method

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Factorial {
     /**
      * Returns the factorial of the argument given
@@ -5,11 +7,37 @@ public class Factorial {
      * @param n the number for which to calculate the factorial
      * @return the factorial
      */
-    public static int factorial(int n) {
+    public static int factorial(int n) throws IllegalArgumentException {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is undefined for negative integers");
+        } else if (n > 16) {
+            throw new IllegalArgumentException("Factorial(" + n + ") is too large - overflow occurs!");
+        } else {
+            int fact = 1;
+            for (int i = n; i > 0; i--)
+                fact *= i;
+            return fact;
+        }
+    }
 
-        int fact = 1;
-        for (int i = n; i > 0; i--)
-            fact *= i;
-        return fact;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean ifContinue = true;
+        while (ifContinue) {
+            System.out.print("Enter an integer: ");
+            int n = scanner.nextInt();
+            try {
+                System.out.println("Factorial(" + n + ") = " + factorial(n));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+
+            System.out.print("nother factorial? (y/n): ");
+            char c = scanner.next().charAt(0);
+            if (c == 'n' || c == 'N') {
+                ifContinue = false;
+            }
+        }
     }
 }
