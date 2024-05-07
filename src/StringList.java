d90c3daf38ca5da7ba3f970afac92b5b1c61ee2b
Carl Mooney <carl.mooney@flinders.edu.au>
2024-05-08 04:20:58 +0930

    intial commit

---
+++ src/StringList.java
@@ -0,0 +1,29 @@
+/**
+ * Class that simulates a list for storing Strings.
+ *
+ * @author << your name goes here >>
+ * @version << version number here >>
+ */
+public class StringList {
+
+    /**
+     * Default initial capacity.
+     */
+    private static final int DEFAULT_CAPACITY = 4;
+
+    /**
+     * The amount to grow this {@code StringList} when {@code grow()} is called.
+     */
+    private static final int GROW = 5;
+    /**
+     * The size of the {@code StringList} (the number of elements it contains).
+     */
+    private final int size = 0;
+    /**
+     * The array buffer into which the elements of the {@code StringList} are stored.
+     */
+    private String[] elementData;
+
+
+}
+
